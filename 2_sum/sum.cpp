03d85f67b8e3003effd06ae53ceec2ec0ab48450
xbz <xbz.o3sig@gmail.com>
2015-01-04 14:33:49 +0800

    bug fix: find v[i],v[j]'s index
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

--- 2_sum/sum.cpp
+++ 2_sum/sum.cpp
@@ -19,7 +19,7 @@ public:
         sort(v.begin(), v.end());
         int i = 0;
         int j = v.size() - 1;
-        while (i <= j) {
+        while (i < j) {
             if (v[i]+v[j] == target)
                 break;
             else if (v[i]+v[j] < target)
@@ -28,8 +28,13 @@ public:
                 j--;
         }
 
-        int idx1 = find(nums.begin(), nums.end(), v[i]) - nums.begin() + 1;
-        int idx2 = find(nums.begin(), nums.end(), v[j]) - nums.begin() + 1;
+        vector<int>::iterator it = find(nums.begin(), nums.end(), v[i]);
+        int idx1 = it - nums.begin() + 1;
+        if (v[i] != v[j])
+            it = nums.begin();
+        else
+            ++it;
+        int idx2 = find(it, nums.end(), v[j]) - nums.begin() + 1;
         int min_idx = min(idx1, idx2);
         int max_idx = max(idx1, idx2);
         vector<int> ret;
@@ -41,11 +46,11 @@ public:
 
 int main(int argc, char *argv[])
 {
-    int arr[] = { 0, 4, 3, 0 };
+    int arr[] = { 5, 75, 25 };
     // int arr[] = { 3, 4, 2, 9, 1, 20, 15, 6 };
     vector<int> v(arr, arr+sizeof(arr)/sizeof(arr[0]));
     Solution s;
-    vector<int> r = s.twoSum(v, 7);
+    vector<int> r = s.twoSum(v, 100);
     print(r);
 
     return 0;
