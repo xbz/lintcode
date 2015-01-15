6de1fb73f354b55f72c386277fb48777e6f30269
xbz <xbz.o3sig@gmail.com>
2015-01-15 11:59:25 +0800

    modify word-segmentation to accept
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

--- word_segmentation/word_seg.cpp
+++ word_segmentation/word_seg.cpp
@@ -10,12 +10,26 @@ public:
         bool f[s.size()+1];
         f[0] = true;
 
+        bool chs[256] = {false};
+        for (unordered_set<string>::iterator it=dict.begin(); \
+             it!=dict.end(); ++it) {
+            string word = *it;
+            for (size_t i=0; i<word.size(); ++i)
+                chs[static_cast<int>(word[i])] = true;
+		}
+        for (size_t i=0; i<s.size(); ++i) {
+            if (!chs[static_cast<int>(s[i])])
+                return false;
+        }
+
         for (size_t i=1; i<=s.size(); ++i) {
             f[i] = false;
-            for (size_t j=0; j<i; ++j) {
+            for (int j=i-1; j>=0; --j) {
                 string sub = s.substr(j, i-j);
-                if (f[j] && dict.find(sub)!=dict.end() && !f[i])
+                if (f[j] && dict.find(sub)!=dict.end() && !f[i]) {
                     f[i] = true;
+                    break;
+                }
             }
         }
         return f[s.size()];
@@ -25,7 +39,7 @@ public:
 int main(int argc, char *argv[])
 {
     Solution s;
-    string str = "lintcodecodelintsum";
+    string str = "xlintcodecodelintsum";
     unordered_set<string> dict;
     dict.insert("lint");
     dict.insert("code");
