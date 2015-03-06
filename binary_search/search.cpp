e8446358e1f362bc1124c06088d394199bbb98c7
xbz <xbz.o3sig@gmail.com>
2015-03-06 18:26:10 +0800

    refactor binary-serach
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

--- binary_search/search.cpp
+++ binary_search/search.cpp
@@ -6,6 +6,39 @@ using namespace std;
 class Solution {
 public:
     int binarySearch(vector<int> &array, int target) {
+        if (array.size() == 0)
+            return -1;
+
+        size_t low = 0;
+        size_t high = array.size() - 1;
+        int index = -1;
+        while (low <= high) {
+            size_t mid = low + (high-low)/2;
+            if (array[mid] == target)
+                index = mid;
+
+            if (mid==0 || mid==array.size()-1)
+                break;
+
+            if (array[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return index;
+    }
+
+    int binarySearch_stl(vector<int> &array, int target) {
+        vector<int>::const_iterator cit = \
+            lower_bound(array.begin(), array.end(), target);
+        if (cit == array.end())
+            return -1;
+        if (*cit > target)
+            return -1;
+        return cit - array.begin();
+    }
+
+    int binarySearch_orig(vector<int> &array, int target) {
         int lower = 0;
         int upper = array.size() - 1;
         int ret = -1;
