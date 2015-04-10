180e798a224a4ed9758bd98dbf22cb212834b880
xbz <xbz.o3sig@gmail.com>
2015-04-10 18:32:16 +0800

    add remove_node_in_binary_search_tree: ugly version
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

---
+++ remove_node_in_binary_search_tree/remove.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <climits>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void print(const vector<int>& v)
+{
+    vector<int>::size_type i;
+    for (i=0; i<v.size()-1; i++)
+        cout << v[i] << ' ';
+    cout << v[i] << endl;
+}
+
+class Traversal {
+public:
+    vector<int> preorder(TreeNode *root) {
+        vector<int> ret;
+        if (root == NULL)
+            return ret;
+
+        stack<TreeNode *> s;
+        s.push(root);
+        while (!s.empty()) {
+            TreeNode *cur = s.top();
+            s.pop();
+            ret.push_back(cur->val);
+            if (cur->right != NULL)
+                s.push(cur->right);
+            if (cur->left != NULL)
+                s.push(cur->left);
+        }
+        return ret;
+    }
+
+    vector<int> inorder(TreeNode *root) {
+        vector<int> ret;
+        stack<TreeNode *> s;
+        TreeNode *cur = root;
+        while (true) {
+            while (cur != NULL) {
+                s.push(cur);
+                cur = cur->left;
+            }
+            if (s.empty())
+                break;
+            cur = s.top();
+            s.pop();
+			ret.push_back(cur->val);
+            cur = cur->right;
+        }
+        return ret;
+    }
+};
+
+class Solution {
+public:
+    TreeNode* removeNode(TreeNode* root, int value) {
+		if (root == NULL)
+			return NULL;
+
+		if (find(root, value) == NULL)
+			return root;
+
+		if (root->val == value) {
+			if (root->left==NULL && root->right==NULL)
+				return NULL;
+			else if (root->left == NULL)
+				return root->right;
+			else if (root->right == NULL)
+				return root->left;
+			else
+				return del_root(root, value);
+		} else {
+			TreeNode* cur = find(root, value);
+			TreeNode* fat = father(root, value);
+			if (cur->left==NULL && cur->right==NULL) {
+				if (fat->left == cur)
+					fat->left = NULL;
+				else
+					fat->right = NULL;
+				return root;
+			}
+			if (cur->left==NULL) {
+				if (fat->left == cur)
+					fat->left = cur->right;
+				else
+					fat->right = cur->right;
+				return root;
+			}
+			if (cur->right==NULL) {
+				if (fat->left == cur)
+					fat->left = cur->left;
+				else
+					fat->right = cur->left;
+				return root;
+			}
+			return del(root, cur, fat, value);
+		}
+    }
+
+private:
+	TreeNode* del(TreeNode* root, TreeNode* cur, TreeNode* fat, int value) {
+		if (cur->right->left == NULL) {
+			cur->right->left = cur->left;
+			if (fat->right == cur)
+				fat->right = cur->right;
+			else
+				fat->left = cur->right;
+		} else {
+			TreeNode* p = cur->right;
+			TreeNode* pre = p;
+			while (p->left != NULL) {
+				pre = p;
+				p = p->left;
+			}
+			pre->left = p->right;
+			p->left = cur->left;
+			p->right = cur->right;
+			if (fat->right == cur)
+				fat->right = p;
+			else
+				fat->left = p;
+		}
+		return root;
+	}
+
+	TreeNode* del_root(TreeNode* root, int value) {
+		if (root->right->left == NULL) {
+			root->right->left = root->left;
+			return root->right;
+		} else {
+			TreeNode* cur = root->right;
+			TreeNode* pre = cur;
+			while (cur->left != NULL) {
+				pre = cur;
+				cur = cur->left;
+			}
+			pre->left = cur->right;
+			cur->left = root->left;
+			cur->right = root->right;
+			return cur;
+		}
+	}
+
+	TreeNode* father(TreeNode* root, int value) {
+		if (root == NULL)
+			return NULL;
+
+		if (root->left == NULL) {
+			if (root->right->val == value)
+				return root;
+			return father(root->right, value);
+		} else if (root->right == NULL) {
+			if (root->left->val == value)
+				return root;
+			return father(root->left, value);
+		} else {
+			if (root->left->val == value)
+				return root;
+			if (root->right->val == value)
+				return root;
+			TreeNode* cur = father(root->left, value);
+			if (cur != NULL)
+				return cur;
+			return father(root->right, value);
+		}
+	}
+
+	TreeNode* find(TreeNode* root, int value) {
+		if (root == NULL)
+			return NULL;
+
+		if (value == root->val)
+			return root;
+		else if (value < root->val)
+			return find(root->left, value);
+		else
+			return find(root->right, value);
+	}
+};
+
+int main(int argc, char *argv[])
+{
+    // TreeNode *empty = NULL;
+    TreeNode *t1 = new TreeNode(5);
+    TreeNode *t2 = new TreeNode(50);
+    TreeNode *t3 = new TreeNode(15);
+    t3->left = t1;
+    t3->right = t2;
+    TreeNode *t4 = new TreeNode(80);
+    TreeNode *t5 = new TreeNode(60);
+    t5->left = t3;
+    t5->right = t4;
+    TreeNode *t6 = new TreeNode(30);
+    t2->left = t6;
+    // TreeNode *t7 = new TreeNode(10);
+    // t1->right = t7;
+
+	Solution s;
+	TreeNode *root = s.removeNode(t5, 90);
+
+	Traversal tra;
+    print(tra.preorder(root));
+	print(tra.inorder(root));
+
+    return 0;
+}
